1c9490a8858da36ef7c0f976442084b4cdb961ca
ricardoteles <ricardo.oliveira.teles@gmail.com>
2017-05-14 08:02:08 -0300

    requisicao get implementada em lista-seminario-alunos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { HttpModule } from '@angular/http';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginAlunoPage } from '../pages/login-aluno/login-aluno';
@@ -26,6 +28,7 @@ import { CadastroProfessorPage } from '../pages/cadastro-professor/cadastro-prof
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/lista-seminario-aluno/lista-seminario-aluno.ts
+++ src/pages/lista-seminario-aluno/lista-seminario-aluno.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import 'rxjs/add/operator/map';
+
+import { Http } from '@angular/http';
 
 /**
  * Generated class for the ListaSeminarioAlunoPage page.
@@ -13,13 +16,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'lista-seminario-aluno.html',
 })
 export class ListaSeminarioAlunoPage {
+  public items:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
   	console.log(navParams.get('val'));
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ListaSeminarioAlunoPage');
+  itemClicked(event,itemData){
+    console.log("item clicked");
+    // console.log(event);
+    console.log(itemData);
+  }
+
+  ionViewDidLoad(){
+  
+  	this.http.get('http://207.38.82.139:8001/seminar').map(res => res.json()).subscribe(data => {
+	  		console.log(data);
+        	this.items = data['data'];
+        	console.log(this.items[0]);
+        	console.log(typeof(this.items));
+        }, error=>{
+            console.log(error);// Error getting the data
+  	});
+
   }
 
 }
